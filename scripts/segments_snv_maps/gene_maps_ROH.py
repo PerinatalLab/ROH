3d9897602bea0d2a89d3acefd558a10ce3b51253
psnavais <psnavais@gmail.com>
2020-01-16 14:01:43 +0100

    generalize code

--- scripts/segments_snv_maps/gene_maps_ROH.py
+++ scripts/segments_snv_maps/gene_maps_ROH.py
@@ -7,32 +7,36 @@ def getOverlap(start0, end0, start1, end1):
 
 g= pd.read_csv(snakemake.input[0], sep= '\t', header= 0)
 d= pd.read_csv(snakemake.input[1], delim_whitespace= True, header= 0)
-fam= pd.read_csv(snakemake.input[2], delim_whitespace=True, header= None)
-d= d.loc[:, ['IID','CHR','POS1','POS2']]
 
+x= [line.strip() for line in open(snakemake.input[2], 'r')]
+fam= [i for i in x if 'fam' in i]
+
+fam= pd.read_csv("".join(fam), delim_whitespace=True, header= None)
 fam.columns= ['FID', 'IID', 'X1', 'X2', 'X3', 'X4']
 
-if 'rott' in snakemake.input[1]:
-        CHR= snakemake.wildcards[1]
-if 'harvest' in snakemake.input[1]:
-        CHR= snakemake.wildcards[2]
-
+d= d.loc[:, ['IID','CHR','POS1','POS2']]
+d.columns= ['IID', 'chr', 'POS1', 'POS2']
 
 df_list= list()
-chr_df= d.loc[d.CHR== int(float(CHR)),:]
 
-for id in set(chr_df.IID):
-	temp_df= chr_df.loc[chr_df.IID== id, :]
-	temp_df= pd.merge(temp_df, g, left_on= ['CHR'], right_on= ['chr'], how= 'inner')
-	temp_df['over']= getOverlap(temp_df.POS1, temp_df.POS2, temp_df.start, temp_df.end)
-	temp_df= temp_df.loc[temp_df.over>0, :]
-	indiv_map= temp_df.groupby(['chr', 'gene']).agg({'over': np.sum})
-	indiv_map.rename(columns= {'over': id}, inplace= True)
-	df_list.append(indiv_map)
+for id in set(d.IID):
+	temp_df= d.loc[d.IID== id, :]
+	df= pd.merge(temp_df, g, on= ['chr'], how= 'outer')
+	df['gene']= df.gene.str.replace('.0', '', regex= False)
+	df['overlap']= getOverlap(df.POS1, df.POS2, df.pos1, df.pos2)
+	df= df.loc[df.overlap>0, :]
+	df['overlap']= 1
+	df_list.append(df)
 
-df= pd.concat(df_list, axis=1)
-df.reset_index(inplace= True)
-df= df.groupby(['chr', 'gene']).sum().reset_index()
+df= pd.concat(df_list)
+df.drop_duplicates(['gene', 'IID'], inplace= True)
+df= df.pivot(index= 'gene', columns= 'IID', values= 'overlap')
+df['gene']= df.index
+
+cols = list(df.columns)
+cols = [cols[-1]] + cols[:-1]
+df = df[cols]
+
 o_ids= fam[~fam.IID.isin(df.columns)]['IID']
 df= pd.concat((df, pd.DataFrame(columns= o_ids, index= df.index)), axis= 1)
 df.fillna(0, inplace= True)
