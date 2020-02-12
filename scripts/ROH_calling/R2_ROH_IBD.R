035dbd46f83aa93d6fb5a0eaf92051b0812db1d2
psnavais <psnavais@gmail.com>
2020-02-12 13:44:44 +0100

    added sUPD check

--- scripts/ROH_calling/R2_ROH_IBD.R
+++ scripts/ROH_calling/R2_ROH_IBD.R
@@ -9,9 +9,9 @@ if (nrow(kin) > 0){
   kin_temp= kin
   colnames(kin_temp)= c("ID2", "ID1", "KINSHIP")
   kin_temp= rbind(kin_temp, kin)
-  kin_temp= kin_temp %>% add_count(ID1)
-  kin_temp= kin_temp %>% add_count(ID2)
-  kin_temp= arrange(kin_temp, n, nn)
+  kin_temp= kin_temp %>% add_count(ID1, name= 'n_ID1')
+  kin_temp= kin_temp %>% add_count(ID2, name= 'n_ID2')
+  kin_temp= arrange(kin_temp, n_ID1, n_ID2)
   to_keep= list()
 
   for (i in 1:nrow(kin_temp)) {
@@ -46,13 +46,31 @@ ibd= full_join(ibd, trio, by= c('Child', 'Mother', 'Father'))
 
 flag= fread(snakemake@input[[5]])
 
+mfr= fread(snakemake@input[[9]])
+
+link= fread(snakemake@input[[10]])
+
 pca_out= fread(snakemake@input[[8]], h=F)
 
 if (grepl('harvest', snakemake@input[[1]])) {
 names(pca)= c('FID', 'IID', 'NMISS_ALLELE_CT', 'NAMED_ALLELE_DOSAGE_SUM', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8', 'PC9', 'PC10')
 flag= rename(flag, coreLMM = coreOK, phenoOK= phenotypesOK)
+link= select(link, PREG_ID_1724, SentrixID_1)
+mfr= inner_join(mfr, link, by= 'PREG_ID_1724')
+
+mfr= mutate(mfr, FLERFODSEL==0 , DODKAT<6 | DODKAT>10, !is.na(SVLEN_UL_DG), SVLEN_UL_DG<308, SVLEN_UL_DG>154, is.na(IVF),ABRUPTIOP==0, PLACENTA_PREVIA==0, FOSTERV_POLYHYDRAMNION==0, C00_MALF_ALL==0, FOSTERV_OLIGOHYDRAMNION== 0, FOSTERV_MISF_STINK_INFI== 0)
+
+flag= filter(flag, IID %in% mfr$SentrixID_1)
+
+
 } else {
 names(pca)= c('FID', 'IID', 'NMISS_ALLELE_CT', 'NAMED_ALLELE_DOSAGE_SUM', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8', 'PC9', 'PC10')
+link= select(link, PREG_ID_315, SentrixID)
+mfr= inner_join(mfr, link, by= 'PREG_ID_315')
+mfr= mutate(mfr, FLERFODSEL== 'Enkeltfødsel' , grepl('Levendefødt', DODKAT), !is.na(SVLEN_UL_DG),SVLEN_UL_DG<308, SVLEN_UL_DG>154, is.na(IVF) | IVF== '',ABRUPTIOP=='Nei', PLACENTA_PREVIA=='Nei', FOSTERV_POLYHYDRAMNION=='Nei', C00_MALF_ALL=='Nei', FOSTERV_OLIGOHYDRAMNION== 'Nei', FOSTERV_MISF_STINK_INFI== 'Nei')
+
+flag= filter(flag, IID %in% mfr$SentrixID)
+
 }
 
 
@@ -73,12 +91,18 @@ infile= list()
 R2= list()
 
 
-
+out_list= snakemake@input[grep('list_', snakemake@input)]
 flist= snakemake@input[grep('.hom.indiv', snakemake@input)]
 
 for (f in flist) {
+	for (outUPD in out_list){
+	if (sub('.txt', '.hom.indiv', sub('list_', '', outUPD)) == f){
+	x= readLines(outUPD)
+	}
+	}
 	infile= c(f, infile)
 	d= fread(f)
+	d= filter(d, !(IID %in% x))
 	d= full_join(d, fam_roh, by= c('IID'))
 	d= inner_join(d, ibd, by= c('IID'= 'Child'))
 
@@ -97,9 +121,9 @@ for (f in flist) {
 	R2= c(r, R2)
 }
 
-d= do.call(rbind, Map(data.frame, R2=R2, file=infile))
+d= do.call(rbind, Map(data.frame, R2= R2, file= infile))
 
-write.table(d, snakemake@output[[1]], sep= '\t', row.names= FALSE, col.names= TRUE, quote= FALSE)
+write.table(d, snakemake@output[[1]], sep= '\t', row.names= T, col.names= FALSE, quote= FALSE)
 
 d= d[order(d$R2, decreasing=T), ]
 
