0fef5d6a23c0e7d65d40479f8ac9eb8f9d96d421
psnavais <psnavais@gmail.com>
2020-03-30 11:20:06 +0200

    mapping

--- scripts/replication/pheno_replication.py
+++ scripts/replication/pheno_replication.py
@@ -3,15 +3,20 @@
 import pandas as pd
 import numpy as np
 
-wild= snakemake.wildcards.rep
+coh= snakemake.wildcards.rep
 
 def pheno():
-	mfr= pd.read_csv(snakemake.input[0], sep= '\t', header= 0)
-	link= pd.read_csv(snakemake.input[1], delim_whitespace= True, header= 0)
-	pca= pd.read_csv(snakemake.input[2], delim_whitespace= True, header= 0)
-	pca.columns= ['FID', 'SentrixID', 'NMISS_ALLELE_CT', 'NAMED_ALLELE_DOSAGE_SUM', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8', 'PC9', 'PC10']
-	mfr= pd.merge(mfr, link, on= ['PREG_ID_315'], how= 'inner')
-	d= pd.merge(mfr, pca, on= ['SentrixID'], how= 'inner')
+	d= pd.read_csv(snakemake.input[1], delim_whitespace= True)
+	mfr= pd.read_csv(snakemake.input[2], sep='\t', header= 0)
+	link= pd.read_csv(snakemake.input[3], delim_whitespace= True, header= 0)
+	bim= [line.strip() for line in open(snakemake.input[4], 'r')]
+	bim= "".join(bim[1])
+	bim= pd.read_csv(bim, sep= '\t', header= None, names=['chr', 'snp', 'cM', 'pos', 'A1', 'A2'])
+	bp= bim.groupby(['chr'])['pos'].diff(1).sum() / 1000000
+	mfr= pd.merge(mfr, link, on= 'PREG_ID_315', how= 'inner')
+	d= pd.merge(d, mfr, left_on= ['IID'], right_on= ['SentrixID'], how= 'inner')
+	d['KB']= d['KB'] / 1000000 * 1000
+	d[['FKB', 'FKBAVG', 'FNSEG']]= d[['KB', 'KBAVG', 'NSEG']].divide(bp, axis=1)
 	d= d[(d['FLERFODSEL']=='Enkeltfødsel')]
 	d= d[d['DODKAT'].str.contains('Levendefødt')]
 	d= d[(d['SVLEN_UL_DG']< 308)]
@@ -19,49 +24,37 @@ def pheno():
 	d= d.dropna(subset= ['SVLEN_UL_DG'])
 	d['IVF']= d['IVF'].replace(' ', np.nan)
 	d= d[(pd.isnull(d['IVF']))]
-	d= d[(d.ABRUPTIOP=='Nei')]
-	d= d[(d.PLACENTA_PREVIA=='Nei') ]
 	d= d[(d.FOSTERV_POLYHYDRAMNION=='Nei')]
 	d= d[(d.FOSTERV_OLIGOHYDRAMNION== 'Nei')]
-        d= d[(d.FOSTERV_MISF_STINK_INFI== 'Nei')]
 	d= d[(d.C00_MALF_ALL=='Nei')]
-	flag= pd.read_csv(snakemake.input[4], sep= '\t', header= 0)
-	flag= flag[(flag['genotypesOK']== True) & (flag['phenoOK']== True) & (flag['coreLMM']== True)]
+	
+	flag= pd.read_csv(snakemake.input[5], sep= '\t', header= 0)
+	flag= flag[(flag['genotypesOK']== True) & (flag['phenoOK']== True)]
 	d= d.loc[d.SentrixID.isin(flag.IID), :]
 	d.drop_duplicates(subset= ['PREG_ID_315'], keep= 'first', inplace= True)
-	pca_out= [line.strip() for line in open(snakemake.input[5], 'rt')]
+	pca_out= [line.strip() for line in open(snakemake.input[6], 'rt')]
+	d['spont']= np.where(((d.FSTART=='Spontan') | (d.FSTART== '')) | (((d.KSNITT=='') | (d.KSNITT== 'Uspesifisert') | (d.KSNITT== 'Akutt keisersnitt')) & (d.INDUKSJON_PROSTAGLANDIN=='Nei') & (d.INDUKSJON_ANNET=='Nei') & (d.INDUKSJON_OXYTOCIN=='Nei') & (d.INDUKSJON_AMNIOTOMI=='Nei')) | (~d.VANNAVGANG.isnull()), 1, 0)
+	d['PARITY0']= np.where(d.PARITET_5=='0 (førstegangsfødende)', 1, 0)
+	sUPD= [line.strip() for line in open(snakemake.input[7], 'rt')]
+	d= d.loc[~d.IID.isin(sUPD), :]
+	d['PREG_ID']= d.PREG_ID_315
+	d['MORS_ALDER']= d.FAAR - d.MOR_FAAR
+	if snakemake.wildcards.sample_rep== 'maternal':
+		d['ALDER']= d.FAAR - d.MOR_FAAR
+	if snakemake.wildcards.sample_rep== 'paternal':
+		d['ALDER']= pd.Categorical(d.FARS_ALDER_KAT_K8).codes + 1
+		d['ALDER']= np.where(d.ALDER== 0, np.nan, d.ALDER)
+	if snakemake.wildcards.sample_rep== 'fetal':
+		d['ALDER']= d.FAAR
+	d['SVLEN_UL_DG']= np.where(d.FKB< 0.08, d.SVLEN_UL_DG, np.nan)
+	d= d.loc[d.VEKT> 1500, :]
+	d['cohort']= coh
 	d= d.loc[~d.SentrixID.isin(pca_out), :]
+	d.drop_duplicates(subset= ['IID'], keep= 'first', inplace= True)
+	d= d[['IID', 'PREG_ID', 'SVLEN_UL_DG', 'spont', 'ALDER', 'cohort', 'FKB', 'NSEG', 'KBAVG', 'PARITY0']]
 	return d
 
-### Relatedness
-
-def selectUnrelated(df, x):
-	kin= pd.read_csv(snakemake.input[3], sep= '\t')
-	kin= kin.loc[kin.KINSHIP > 0.0884, :]
-	kin= kin.loc[kin.ID1.isin(x.values)]
-	kin= kin.loc[kin.ID2.isin(x.values)]
-	kin= kin.loc[:, ['ID1','ID2','KINSHIP']]
-	kin_temp= kin.copy()
-	kin_temp.columns= ['ID2', 'ID1', 'KINSHIP']
-	kin_temp= kin_temp.append(kin)
-	kin_temp['n']= kin_temp.groupby('ID1')['ID1'].transform('count')
-	kin_temp['nn']= kin_temp.groupby('ID2')['ID2'].transform('count')
-	kin_temp.sort_values(by=['n', 'nn'], inplace= True)
-	to_keep= list()
-	for i in range(0, len(kin_temp.index)):
-		if kin_temp.iloc[i, 0] in kin_temp.iloc[0:i, 1].values:
-			kin_temp.iloc[i, 1]= "X"
-		else:
-			to_keep.append(kin_temp.iloc[i, 0])
-	to_remove= [i for i in kin_temp.ID1 if i not in to_keep]
-	to_remove= list(set(to_remove))
-	remove= pd.DataFrame({'FID': to_remove})
-	remove['IID']= remove.FID
-	return remove
-
 d= pheno()
-remove= selectUnrelated(d, d.SentrixID)
-d= d[~d.SentrixID.isin(remove)]
 
 d.to_csv(snakemake.output[0], sep= '\t', index= False)
 
